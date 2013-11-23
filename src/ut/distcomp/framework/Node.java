5a96b0edbbb5d09ec0561f58f58e493847db0eb1
princessofpersia <emailnazneen@gmail.com>
2013-11-23 00:30:26 -0600

    Added ndoe skeleton

--- src/ut/distcomp/framework/Node.java
+++ src/ut/distcomp/framework/Node.java
@@ -1,5 +1,32 @@
 package ut.distcomp.framework;
 
-public class Node extends Thread{
+import java.util.ArrayList;
+import java.util.List;
 
+public class Node extends Thread{
+	int node_id;
+	List<updates> log;
+	static PlayList db;
+	static List<Boolean> connections;
+	
+	public Node(int nodeId){
+		this.node_id=nodeId;
+		connections=new ArrayList<Boolean>();
+	}
+	
+	public void add_entry(){
+		//TODO: add to log, called by controller
+	}
+	
+	public void truncate_log(){
+		//TODO: remove logs that are stable
+	}
+	
+	public void anti_entropy(Node node){
+		//TODO: gossip protocol
+	}
+	
+	public void retire(){
+		//TODO: transfer db and leave
+	}
 }
